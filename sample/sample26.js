9b9d7ed2f3f040614a9ef40a0e1d3eb804fe3660
Chae Seong Lim <pan1129@naver.com>
2023-10-20 20:42:00 +0900

    일반 예외처리하기

---
+++ sample/sample26.js
@@ -0,0 +1,17 @@
+function f2() {
+    console.log('this is f2 start');
+    throw new Error('오류');
+    console.log('this is f2 end');
+}
+
+function f1() {
+    console.log('this is f1 start');
+    try {
+        f2();
+    } catch (e) {
+        console.log(e);
+    }
+    console.log('this is f1 end');
+}
+
+f1();
